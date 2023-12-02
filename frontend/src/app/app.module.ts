c477d7e07364fa1295bf7692b93d0c0502f01b4a
knopp <knopp@pads.rwth-aachen.de>
2023-12-02 13:51:44 +0100

    updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
@@ -9,6 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { ModelViewComponent } from './model-view/model-view.component'; // Import HttpClientModule
 import { NgSelectModule } from '@ng-select/ng-select';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
